708504ad47e9359bec40e79e5bd4939bab6485b6
Christian Fei <crifei93@gmail.com>
2017-04-20 14:27:08 +0200

    add resources for author and book

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418122839) do
+ActiveRecord::Schema.define(version: 20170420122432) do
 
   create_table "authors", force: :cascade do |t|
-    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "books", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "published_at"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
